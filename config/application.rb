11441424f3e6902886f40e863ffce8da43da79ac
Ubuntu <fukitaseiya@gmail.com>
2022-12-29 14:22:21 +0000

    Render deploy Setting

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.active_storage.variant_processor = :mini_magick
+    config.hosts << 'xxx.onrender.com'
   end
 end
